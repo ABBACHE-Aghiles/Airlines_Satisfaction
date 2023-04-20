ea2b149892eddab3a2d048a4a6948c4e88549818
ABBACHE-Aghiles <125451687+ABBACHE-Aghiles@users.noreply.github.com>
2023-04-20 22:49:55 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,16 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-# Configuration de Seaborn pour Streamlit
-sns.set_theme(color_codes=True)
 
-# Configuration de pandas pour afficher toutes les colonnes
 st.set_option('deprecation.showPyplotGlobalUse', False)
+sns.set_theme(color_codes=True)
 pd.set_option('display.max_columns', None)
-
-st.set_page_config(layout="wide")
-# Afficher le titre en haut et centré
-st.markdown("<h1 style='text-align: center;'>Data Slayers ⚔️</h1>", unsafe_allow_html=True)
 
 # Importer les données
 @st.cache
@@ -69,3 +63,76 @@ st.pyplot(fig3)
 fig4 = plt.figure(figsize=(8,8),dpi=100)
 sns.scatterplot(x="Departure Delay in Minutes", y="Arrival Delay in Minutes", hue="satisfaction", data=df, edgecolor="black")
 st.pyplot(fig4)
+
+
+
+Aghiles ABBACHE
+je veu meme les shape etc
+Voici le code mis à jour avec les informations de forme affichées :
+
+scss
+Copy code
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.set_page_config(page_title="Data Slayers ⚔️")
+
+st.title("Exploration de données pour la satisfaction des passagers aériens")
+
+df = pd.read_csv('Data.csv')
+
+st.write("Shape of the dataset: ", df.shape)
+st.write("_______________________________________")
+st.write("Number of null values: ")
+st.write(df.isnull().sum())
+st.write("_______________________________________")
+st.write("Number of unique values: ")
+st.write(df.nunique())
+
+st.write("## Supprimer les colonnes 'Unnamed: 0' et 'id'")
+
+df.drop(columns=['Unnamed: 0', 'id'], inplace=True)
+
+st.write("Nouvelle forme du jeu de données après suppression des colonnes : ", df.shape)
+
+st.write("## Analyse exploratoire des données")
+
+st.write("### Proportion de satisfaction")
+
+satisfaction_counts = df.satisfaction.value_counts()
+satisfaction_piechart = plt.pie(satisfaction_counts, labels = ["Neutral or dissatisfied", "Satisfied"], colors = sns.color_palette("YlOrBr"), autopct = '%1.1f%%')
+st.pyplot(satisfaction_piechart.figure)
+
+st.write("### Satisfaction en fonction des variables catégorielles")
+
+cat_vars = ['Gender', 'Customer Type', 'Type of Travel', 'Class']
+
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(15, 15))
+axs = axs.flatten()
+
+for i, var in enumerate(cat_vars):
+    sns.countplot(x=var, hue='satisfaction', data=df, ax=axs[i])
+    axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=90)
+
+fig.tight_layout()
+
+st.pyplot(fig)
+
+st.write("### Top 10 des tranches d'âge les plus voyageuses")
+
+age_count = df['Age'].value_counts(ascending=False).head(10)
+age_barplot = px.bar(y=age_count.values, 
+             x=age_count.index, 
+             color = age_count.index,
+             color_discrete_sequence=px.colors.sequential.PuBuGn,
+             text=age_count.values,
+             title= "Top 10 des tranches d'âge les plus voyageuses :",
+             template= 'plotly_dark')
+age_barplot.update_layout(
+    xaxis_title="Âge",
+    yaxis_title="Nombre de voyages",
+    font = dict(size=20,family="Franklin Gothic"))
+st.plotly_chart(age_barplot)
