b45d162393f26dcfcbdf3f29fa01304044a07209
ABBACHE-Aghiles <a_abbache@hetic.eu>
2023-04-20 22:32:43 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.title("Data Slayers ⚔️: La plateforme ultime pour la visualisation et l'analyse de données")
+st.title("Data Slayers ⚔️: la plateforme ultime pour la visualisation et l'analyse de données")
+
+# Afficher le titre en haut et centré
+st.markdown("<h1 style='text-align: center;'>Data Slayers ⚔️</h1>", unsafe_allow_html=True)
